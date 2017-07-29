95b651935e8d209b1beb6486b215e4110d4ea3c3
Sipos Zoltan <cafeaua@gmail.com>
2017-07-29 15:19:59 +0300

    Names Dialog cu exemplu.

---
+++ NamesDialog/src/NamesDialog.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+
+/**
+ * Using dialogs with JOptionPane
+ */
+public class NamesDialog {
+    public static void main(String[] args) {
+        //Declaring String Variables
+        String firstName;
+        String middleName;
+        String lastName;
+
+        // Get user's first name;
+        firstName =
+                JOptionPane.showInputDialog("What is " +
+                        "your first name? ");
+
+        // Get user's second name;
+        middleName =
+                JOptionPane.showInputDialog("What is " +
+                        "your middle name? ");
+
+        // Get user's last name;
+        lastName =
+                JOptionPane.showInputDialog("What is " +
+                        "your last name? ");
+
+        //Display a greeting.
+        JOptionPane.showMessageDialog(null, "Hello " +
+                firstName + " " + middleName + " " + lastName + " ");
+
+        System.exit(0);
+
+    }
+}
